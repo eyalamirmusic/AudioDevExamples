456f92b3644af8510d3f6db782672baa20a50f6d
Eyal Amir <eyalamirmusic@gmail.com>
2020-01-16 16:57:11 +0200

    Attempted style change on ConsoleToot to make it a bit more "JUCE-like"

--- Standalone/ConsoleToot/Source/Main.cpp
+++ Standalone/ConsoleToot/Source/Main.cpp
@@ -1,54 +1,64 @@
-#include "../JuceLibraryCode/JuceHeader.h"
+#include "JuceHeader.h"
 
 class MyCallback : public AudioIODeviceCallback
 {
 public:
-	MyCallback() {}
-	void audioDeviceIOCallback(const float ** inputChannelData, 
-		int numInputChannels, float ** outputChannelData, int numOutputChannels, int numSamples) override
-	{
-		for (int i = 0; i < numSamples; ++i)
-		{
-			float s = m_gain * sin(m_phase);
-			for (int j = 0; j < numOutputChannels; ++j)
-			{
-				outputChannelData[j][i] = s;
-			}
-			m_phase += (m_freq / m_sr)*juce::MathConstants<double>::twoPi;
-		}
-	}
+    void audioDeviceIOCallback(const float** inputChannelData,
+                               int numInputChannels,
+                               float** outputChannelData,
+                               int numOutputChannels,
+                               int numSamples) override
+    {
+        for (int i = 0; i < numSamples; ++i)
+        {
+            float s = gain * sin(phase);
 
-	void audioDeviceAboutToStart(AudioIODevice * device) override
-	{
-		m_phase = 0.0;
-		m_sr = device->getCurrentSampleRate();
-	}
+            for (int j = 0; j < numOutputChannels; ++j)
+            {
+                outputChannelData[j][i] = s;
+            }
 
-	void audioDeviceStopped() override
-	{
-	}
-	double m_freq = 440.0;
-	double m_gain = 0.1;
+            phase += (freq / sampleRate) * juce::MathConstants<float>::twoPi;
+        }
+    }
+
+    void audioDeviceAboutToStart(AudioIODevice* device) override
+    {
+        phase = 0.0;
+        sampleRate = (float) device->getCurrentSampleRate();
+    }
+
+    void audioDeviceStopped() override {}
+
 private:
-	double m_phase = 0.0;
-	double m_sr = 0.0;
+    float freq = 440.f;
+    float gain = 0.1f;
+    float phase = 0.0;
+    float sampleRate = 0.0;
 };
 
-int main (int argc, char* argv[])
+int main(int argc, char* argv[])
 {
-	ScopedJuceInitialiser_GUI gui_init; // not used for "GUI" in this case, just for initing and shutting down the MessageManager
-	AudioDeviceManager aman;
-	String err = aman.initialiseWithDefaultDevices(0, 2);
-	if (err.isEmpty())
-	{
-		std::cout << "device opened : " << aman.getCurrentAudioDevice()->getName() << "\n";
-		MyCallback cb;
-		aman.addAudioCallback(&cb);
-		Thread::sleep(2000); // keep the program running so the audio has a chance to play
-		std::cout << "closing device...\n";
-	}
-	else
-		std::cout << "could not open device : " << err << "\n";
+    // not used for "GUI" in this case,
+    // just for initing and shutting down the MessageManager
+    ScopedJuceInitialiser_GUI gui_init;
+    AudioDeviceManager aman;
+
+    String err = aman.initialiseWithDefaultDevices(0, 2);
+
+    if (err.isEmpty())
+    {
+        std::cout << "device opened : " << aman.getCurrentAudioDevice()->getName()
+                  << "\n";
+        MyCallback cb;
+        aman.addAudioCallback(&cb);
+
+        // keep the program running so the audio has a chance to play:
+        Thread::sleep(2000);
+        std::cout << "closing device...\n";
+    }
+    else
+        std::cout << "could not open device : " << err << "\n";
+
     return 0;
 }
-
