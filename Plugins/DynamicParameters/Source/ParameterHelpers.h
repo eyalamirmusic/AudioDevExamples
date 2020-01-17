6f19627aa8fc036701d97928267f6152b4b10b43
Eyal Amir <eyalamirmusic@gmail.com>
2020-01-17 17:02:15 +0200

    Fixed documentation to dynamic parameters example

--- Plugins/DynamicParameters/Source/ParameterHelpers.h
+++ Plugins/DynamicParameters/Source/ParameterHelpers.h
@@ -3,8 +3,8 @@
 #include "JuceHeader.h"
 
 /*
-  A little helper class to simplify creating a new list, caching it and saving it
-  to a parameter layout.
+  A little helper class to simplify creating a new parameter list,
+  caching it and saving it to a parameter layout.
 
   Use it in a scoped way:
 
@@ -20,12 +20,15 @@ class ParameterList
 {
 public:
     //Constructor: stores a reference to the ParameterLayout
+    //During the destructor, all parameters added using add() or addNamed()
+    //Will be added to the layout
     explicit ParameterList(
         AudioProcessorValueTreeState::ParameterLayout& layoutToUse)
         : layout(layoutToUse)
     {
     }
 
+    //Destructor: only here values will be added to the layout
     ~ParameterList() { layout.add(params.begin(), params.end()); }
 
     //Adds the parameter to the list, returning a pointer to it
@@ -39,7 +42,7 @@ public:
         return newParam;
     }
 
-    //Similar to add(), but also copies the value used for parameterID
+    //Similar to add(), but also copies the value used for parameterID field
     //into the name field, to avoid code duplication
     template <typename T, typename ParamName, typename... Args>
     T* addNamed(const ParamName& name, Args&&... args)
